2835ae2f8e97107ee4a984d7583142a08896d9a8
I-Georg <i.georgieva.18@aberdeen.ac.uk>
2019-12-06 17:37:37 +0000

    added total price and deletion button.slight formatting of the table

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,7 +54,9 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+#source rails5 book
+    @cart.destroy  if @cart.id == session[:cart_id]
+     session[:cart_id] = nil
     respond_to do |format|
       format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
       format.json { head :no_content }
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,9 @@
 class LineItem < ApplicationRecord
   belongs_to :chocolate_bar
   belongs_to :cart
+
+def total_price
+chocolate_bar.price * quantity
+end 
+
 end
