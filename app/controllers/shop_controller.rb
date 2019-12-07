7818e3be150c0245b14323b30a4d4a482ef00c39
I-Georg <i.georgieva.18@aberdeen.ac.uk>
2019-12-07 19:08:25 +0000

    installed will_paginated,working pagination and css

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,5 +1,8 @@
 class ShopController < ApplicationController
+
   def index
   @chocolate_bars = ChocolateBar.order(:bar_name)
+ #pagination,will_paginate gem source https://github.com/mislav/will_paginate
+  @chocolate_bars = ChocolateBar.paginate(page: params[:page], per_page: 35) 
   end
 end
