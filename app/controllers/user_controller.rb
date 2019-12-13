fe781ba8f8da303928b827fa55a1efb38b8dba38
I-Georg <i.georgieva.18@aberdeen.ac.uk>
2019-12-13 15:54:10 +0000

    fixed profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,15 +2,12 @@ class UserController < ApplicationController
 #https://stackoverflow.com/questions/17449573/user-profile-pages-with-devise-routing-to-show-action
 before_action :authenticate_user!
 def show
-    @user = User.find(params[:id])
+    @user = User.all
+#find(params[:id])
 
-    respond_to do |format|
-        format.html { redirect_to @user, notice: 'User login successful' }
-        format.xml { render :xml => @user }
-        format.json { render :show, status: :ok, location: @user}
-
+    
         
     end
-  end
+  
 
 end
